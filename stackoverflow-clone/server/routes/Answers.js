0440344ac006592b657be6b886e90e6b2c71b928
megha-joshi <megha@digitalapple.in>
2023-01-22 18:57:49 +0530

    first commit

---
+++ stackoverflow-clone/server/routes/Answers.js
@@ -0,0 +1,13 @@
+import express from 'express'
+
+import { postAnswer, deleteAnswer } from '../controllers/Answers.js'
+import auth from '../middlewares/auth.js'
+
+
+
+const router = express.Router();
+
+router.patch('/post/:id', postAnswer)
+router.patch('/delete/:id', auth, deleteAnswer)
+
+export default router
